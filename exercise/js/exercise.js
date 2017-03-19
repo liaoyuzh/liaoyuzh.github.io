a189a889e3d764fea0536552c9bbaf69194e7716
TerrenceLiao <terrenceliao@hotmail.com>
2017-03-19 17:24:13 -0400

    modified js

--- exercise/js/exercise.js
+++ exercise/js/exercise.js
@@ -1,39 +1,47 @@
 
-// function main(){
-// 	//displayTime();
-// 	printButton();
-// 	window.onload = function() {
-// 		var date = displayDate();
-// 	  	document.getElementById('practice1').innerHTML = "Date: ";
-// }
+function displayDate(){
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
 
-// function displayTime(){
+	if(dd<10) {
+	    dd='0'+dd
+	} 
 
-// }
+	if(mm<10) {
+	    mm='0'+mm
+	} 
 
-// window.onload = function() {
-// 	var date = displayDate();
-// 	document.getElementById('practice1').innerHTML = "Date: " + date;
+	today = mm+'/'+dd+'/'+yyyy;
+	return today;
+}
 
-// function displayDate(){
-// 	var today = new Date();
-// 	var dd = today.getDate();
-// 	var mm = today.getMonth()+1; //January is 0!
-// 	var yyyy = today.getFullYear();
+function displayTime(){
+	var today = new Date();
+	var hour = today.getHours();
+	var minute = today.getMinutes();
+	var second = today.getSeconds();
+	var time = hour + " : " + minute + " : "+ second;
+	return time
+}
 
-// 	if(dd<10) {
-// 	    dd='0'+dd
-// 	} 
+window.onload = function() {
+	document.getElementById('practice1').innerHTML = "Date: " + displayDate();
+	document.getElementById('practice3').innerHTML = "Time: " + displayTime();
 
-// 	if(mm<10) {
-// 	    mm='0'+mm
-// 	} 
+}
 
-// 	today = mm+'/'+dd+'/'+yyyy;
-// 	return today;
-// }
+function findArea(){
+	var side1 = document.getElementById('side1').value;
+	var side2 = document.getElementById('side2').value;
+	var side3 = document.getElementById('side3').value;
+	var perimeter = (side1 + side2 + side3) / 2;
+	var area =  Math.sqrt(perimeter*((perimeter - side1) * (perimeter - side2) * (perimeter - side3)));
+	document.getElementById('area').innerHTML = "Calculated Area: " + area;
+}
 
-
-function myFunction() {
+function printButton() {
     window.print();
 }
+
